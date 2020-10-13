2499f00dadf94e30953cb81a194a13e64cae4ae0
cloud-del-dev <72771351+cloud-del-dev@users.noreply.github.com>
2020-10-13 15:44:35 +1100

    Add files via upload

---
+++ pull-services-script.ps1
@@ -0,0 +1,23 @@
+#list & save the output of roles to a json file
+aws iam list-roles | Out-File -FilePath .\output.json
+
+#Set output to a readable file
+$s = Get-Content -Raw "output.json" | ConvertFrom-Json 
+
+foreach ($role in $s.roles) {
+  $effect = $role.AssumeRolePolicyDocument.Statement.Effect
+  $serviceid = $role.AssumeRolePolicyDocument.Statement.Principal.Service
+  $service = "$($serviceid)".TrimEnd(".amazonaws.com")
+  ForEach-Object {
+    Write-Output "## $service  "
+    Write-Output "  "
+    Write-Output "Description = TBD  "
+    Write-Output "  "
+    Write-Output "Effect = $effect"
+    Write-Output "  "
+  } | Out-File -FilePath .\"$($service)".md
+}
+
+
+
+
